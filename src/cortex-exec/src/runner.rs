edce8e4d7783c9a586bb35f20e4d1401cd2088c1
echobt <mathis.massimino+echo@cortex.foundation>
2026-02-04 16:01:21 +0000

    fix: apply rustfmt formatting fixes

--- src/cortex-exec/src/runner.rs
+++ src/cortex-exec/src/runner.rs
@@ -192,7 +192,11 @@ impl ExecRunner {
             self.client = Some(client);
         }
 
-        Ok(self.client.as_ref().expect("Client should be initialized in init_client").as_ref())
+        Ok(self
+            .client
+            .as_ref()
+            .expect("Client should be initialized in init_client")
+            .as_ref())
     }
 
     /// Get filtered tool definitions based on options.
